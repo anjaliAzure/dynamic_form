d0a203fe4e705bf9a964cac802032cb27938964c
dhavalazure <dhaval.parmar@azurephygital.com>
2023-06-09 15:03:40 +0530

    Added Paging logic

--- lib/app_constants/constants.dart
+++ lib/app_constants/constants.dart
@@ -1,5 +1,17 @@
 class Constants
 {
+
+  /// Json keys
+  static const fieldsKey = "fields";
+  static const pageKey = "page";
+  static const listsKey = "lists";
+  static const typeKey = "type";
+  static const idKey = "id";
+  static const objectKey = "ob";
+  static const labelKey  = "label";
+  static const validationKey = "validation";
+
+  /// Field type keys
   static const shortText = "short_text";
   static const longText = "long_text";
   static const radio = "radio";
